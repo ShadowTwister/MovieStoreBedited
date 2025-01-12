bf76ccb71e04fa709ee2e1d42747340fe3b3d4af
Filip <filipfilev23@abv.bg>
2025-01-12 23:27:09 +0200

    edit

---
+++ MovieStore-main/MovieStoreB/MovieStoreB.DL/Repositories/MongoRepositories/ActorsRepository.cs
@@ -0,0 +1,18 @@
+﻿using MovieStoreB.DL.Interfaces;
+using MovieStoreB.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieStoreB.DL.Repositories.MongoRepositories
+{
+    internal class ActorsRepository : IActorRepository
+    {
+        public Actor? GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
